2848e7b31c6b94b91f9c8bcb8f6bdfb27a232609
mwmajew <maciej.w.majewski@gmail.com>
2018-03-12 18:52:17 +0100

    Create task.py

---
+++ task.py
@@ -0,0 +1,23 @@
+# Class diary  
+#
+# Create program for handling lesson scores.
+# Use python to handle student (highscool) class scores, and attendance.
+# Make it possible to:
+# - Get students total average score (average across classes)
+# - get students average score in class
+# - hold students name and surname
+# - Count total attendance of student
+# The default interface for interaction should be python interpreter.
+# Please, use your imagination and create more functionalities. 
+# Your project should be able to handle entire school.
+# If you have enough courage and time, try storing (reading/writing) 
+# data in text files (YAML, JSON).
+# If you have even more courage, try implementing user interface.
+#If you can thing of any other features, you can add them.
+#Make intelligent use of pythons syntactic sugar (overloading, iterators, generators, etc)
+#Most of all: CREATE GOOD, RELIABLE, READABLE CODE.
+#The goal of this task is for you to SHOW YOUR BEST python programming skills.
+#Impress everyone with your skills, show off with your code.
+#
+#Your program must be runnable with command "python task.py".
+#Show some usecases of your library in the code (print some things)
